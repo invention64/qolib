49de81dddf082eb840a0005f31bb679841f52eb2
Sean M. Mitchell <seanmmitchell103@gmail.com>
2019-01-26 00:17:34 -0500

    TCP & UDP tested and working!!!

--- qonet.c
+++ qonet.c
@@ -4,7 +4,7 @@
 #include <netinet/in.h>
 #include <string.h>
 
-TCPClient OpenTCP(char* dstIP, unsigned short dstPort) {
+IP4 GenerateIPv4(char* dstIP, unsigned short dstPort) {
     // Validate VARs
     if(dstPort < 0 || dstPort > 50000) {
         qol_cab("Failed to parse destination port. Port must be less than 50K and more than 0.\r\n");
@@ -13,6 +13,20 @@ TCPClient OpenTCP(char* dstIP, unsigned short dstPort) {
     // Target
     struct sockaddr_in dst;
 
+    // Set DST
+    struct in_addr ip;
+    ip.s_addr = inet_addr(dstIP);
+    //printf("IP: %ld\r\n", ip.s_addr);
+    dst.sin_family = AF_INET;
+    dst.sin_addr = ip;
+    dst.sin_port = htons(dstPort);
+
+    IP4 target;
+    target.dst = dst;
+    return target;
+}
+
+TCPClient OpenTCP(IP4 target) {
     // Local Socket
     //printf("Generating socket...\r\n");
     int sock = socket(AF_INET, SOCK_STREAM, 0); 
@@ -23,17 +37,8 @@ TCPClient OpenTCP(char* dstIP, unsigned short dstPort) {
         //printf("Socket Generated!\r\n");
     }
 
-    // Set DST
-    struct in_addr ip;
-    ip.s_addr = inet_addr(dstIP);
-    //printf("IP: %ld\r\n", ip.s_addr);
-    dst.sin_family = AF_INET;
-    dst.sin_addr = ip;
-    dst.sin_port = htons(dstPort);
-
-
     // Connect Local Socket
-    if(connect(sock, &dst, sizeof(dst)) > 0) {
+    if(connect(sock, &target.dst, sizeof(target.dst)) > 0) {
         error("Failed to connect..........");
     } else {
         //printf("Connected!\r\n");
@@ -45,14 +50,14 @@ TCPClient OpenTCP(char* dstIP, unsigned short dstPort) {
 
 int SendArrayTCP(TCPClient client, char* data) {
     int sock = client.socket;
-    return write(sock, data, strlen(data));
+    return send(sock, data, strlen(data), 0);
 }
 
 char* RecieveArrayTCP(TCPClient client, int bufferSize) {
     int sock = client.socket;
     char buffer[bufferSize];
     bzero(buffer, bufferSize);
-    return read(sock, buffer, bufferSize);
+    return recv(sock, buffer, bufferSize, 0);
 }
 
 void CloseTCP(TCPClient client) {
@@ -60,3 +65,36 @@ void CloseTCP(TCPClient client) {
     close(sock);
 }
 
+UDPClient OpenUDP() {    
+    // Local Socket
+    //printf("Generating socket...\r\n");
+    int sock = socket(AF_INET, SOCK_DGRAM, 0); 
+    
+    if (sock < 0) {
+        qol_cab("Failed to create socket.\r\n");
+    } else {
+        //printf("Socket Generated!\r\n");
+    }
+
+    //printf("Connected!\r\n");
+    UDPClient client;
+    client.socket = sock;
+    return client;
+}
+
+int SendArrayUDP(UDPClient client, IP4 target, char* data) {
+    int sock = client.socket;
+    return sendto(sock, data, strlen(data), 0, &target.dst, sizeof(target.dst));
+}
+
+char* RecieveArrayUDP(UDPClient client, IP4 target, int bufferSize) {
+    int sock = client.socket;
+    char buffer[bufferSize];
+    bzero(buffer, bufferSize);
+    return recvfrom(sock, buffer, bufferSize, 0, &target.dst, sizeof(target.dst));
+}
+
+void CloseUDP(UDPClient client) {
+    int sock = client.socket;
+    close(sock);
+}
--- qonet.h
+++ qonet.h
@@ -1,6 +1,28 @@
+#include <netinet/in.h>
+
+typedef struct IP4 {
+    struct sockaddr_in dst;
+} IP4;
+
 typedef struct TCPClient {
     int socket;
-
 } TCPClient;
 
-TCPClient OpenTCP(char* dstIP, unsigned short dstPort);
+typedef struct UDPClient {
+    int socket;
+} UDPClient;
+
+// General
+IP4 GenerateIPv4(char* dstIP, unsigned short dstPort);
+
+// TCP
+TCPClient OpenTCP(IP4 target);
+int SendArrayTCP(TCPClient client, char* data);
+char* RecieveArrayTCP(TCPClient client, int bufferSize);
+void CloseTCP(TCPClient client);
+
+// UDP
+UDPClient OpenUDP();
+int SendArrayUDP(UDPClient client, IP4 target, char* data);
+char* RecieveArrayUDP(UDPClient client, IP4 target, int bufferSize);
+void CloseUDP(UDPClient client);
