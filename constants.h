ec6cea480329b122f59e0af401f93344cfc00a53
invention64 <jimmygottshall@gmail.com>
2019-01-28 16:59:36 -0500

    Moved qol_num struct to constants.h

--- constants.h
+++ constants.h
@@ -28,4 +28,20 @@
  __typeof__ (b) _b = (b); \
  _a < _b ? _a : _b; })
 
+typedef union qol_num qol_num;
+
+//When we have a reference this needs to be front page
+typedef union qol_num {
+	//Unions require knowing the type, though every
+	//generic in C requires knowing the type 
+	int i; //stores 4 bytes -2 mil to +2 mil
+	int f; //stores 4 bytes including point
+
+	//Next 2 are 8 bytes, useful but maybe make it a toggle using define
+	//problem with having these in the union means that no matter
+	//what is stored the size allocated will always be 8 bytes
+	long long int lli; 	//Stores an 8 bit integer
+	double d;						//Stores an 8 bit float
+} qol_num;
+
 #endif
